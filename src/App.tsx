44d28ba208bc7655f5e0f48e057d2c856476012a
AlexandreDev <eztalenda@gmail.com>
2023-10-05 14:28:58 -0300

    feat(routing): Configured React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/user/:username" element={<div>User Profile</div>} />
+        <Route path="/user/:username/repo/:repo" element={<div>Repo Details</div>} />
+      </Routes>
+    </Router>
+  )
 }
-
-export default App;
