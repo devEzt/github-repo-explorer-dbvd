28c1c7f626ff8b70db3b5452943348f976b09a7e
AlexandreDev <eztalenda@gmail.com>
2023-10-06 02:00:27 -0300

    feat(repo-list): Implemented repository sorting options

--- src/Components/RepoList.tsx
+++ src/Components/RepoList.tsx
@@ -9,13 +9,29 @@ const RepoList: React.FC<RepoListProps> = ({ username }) => {
   const [repos, setRepos] = useState<any[]>([])
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
+  const [sortOrder, setSortOrder] = useState<string>('stars-desc')
+
+  const sortRepos = (repos: any[], order: string) => {
+    switch (order) {
+      case 'stars-desc':
+        return repos.sort((a, b) => b.stargazers_count - a.stargazers_count)
+      case 'stars-asc':
+        return repos.sort((a, b) => a.stargazers_count - b.stargazers_count)
+      case 'recent':
+        return repos.sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
+      case 'oldest':
+        return repos.sort((a, b) => new Date(a.updated_at).getTime() - new Date(b.updated_at).getTime())
+      default:
+        return repos
+    }
+  }
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true)
         const response = await getUserRepos(username)
-        setRepos(response)
+        setRepos(sortRepos(response, sortOrder))
         setLoading(false)
       } catch (error) {
         console.error('Erro ao buscar repositórios:', error)
@@ -25,7 +41,7 @@ const RepoList: React.FC<RepoListProps> = ({ username }) => {
     }
 
     fetchData()
-  }, [username])
+  }, [username, sortOrder])
 
   if (!username) {
     return <div>Please enter a username to search.</div>
@@ -41,6 +57,14 @@ const RepoList: React.FC<RepoListProps> = ({ username }) => {
 
   return (
     <div>
+      <select onChange={(e) => setSortOrder(e.target.value)} value={sortOrder}>
+        <option value="stars-desc">Stars Descending</option>
+        <option value="stars-asc">Stars Ascending</option>
+        <option value="default">Default</option>
+        <option value="recent">Most Recent</option>
+        <option value="oldest">Oldest</option>
+      </select>
+
       {repos && repos.length > 0 ? (
         repos.map((repo) => <div key={repo.id}>{repo.name}</div>)
       ) : (
--- src/Pages/UserPage.tsx
+++ src/Pages/UserPage.tsx
@@ -1,20 +1,24 @@
 // UserPage.tsx
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import UserProfile from '../Components/UserProfile'
 import RepoList from '../Components/RepoList'
 
 const UserPage: React.FC = () => {
-  console.log('UserPage mounted')
-
   const { username = '' } = useParams<{ username: string }>()
-  console.log('Username from params:', username)
+  const navigate = useNavigate()
 
   return (
     <div>
       <UserProfile username={username} />
       <RepoList username={username} />
-      <button onClick={() => {}}>Voltar</button>
+      <button
+        onClick={() => {
+          navigate(-1)
+        }}
+      >
+        Voltar
+      </button>
     </div>
   )
 }
