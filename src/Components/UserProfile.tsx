508c4aadd3372d29e9aa134cb86e24b2f5a1175c
AlexandreDev <eztalenda@gmail.com>
2023-10-06 05:56:57 -0300

    feat(UserProfile): Add email and follower details with icons
    - Added email field to UserDetails type and state
    - Included Material-UI icons for email, followers, and following
    - Adding Not Found Image

--- src/Components/UserProfile.tsx
+++ src/Components/UserProfile.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { Avatar, Typography, Paper, Box, Grid, CircularProgress } from '@mui/material'
 import { getUserDetails } from '../Services/getUserDetails'
+import EmailIcon from '@mui/icons-material/Email'
+import PeopleIcon from '@mui/icons-material/People'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
 
 interface UserProfileProps {
   username: string
@@ -12,6 +15,7 @@ interface UserDetails {
   following: number
   avatar_url: string
   bio: string
+  email: string
 }
 
 const UserProfile: React.FC<UserProfileProps> = ({ username }) => {
@@ -55,9 +59,37 @@ const UserProfile: React.FC<UserProfileProps> = ({ username }) => {
           <Grid item>
             <Avatar src={userDetails.avatar_url} alt={`${userDetails.login} avatar`} sx={{ width: 160, height: 160 }} />
           </Grid>
-          <Grid item>
+          <Grid item xs={12}>
             <Typography variant="h3">{userDetails.login}</Typography>
-            <Typography variant="h4">{userDetails.bio || 'Sem Descrição'}</Typography>
+            <Typography variant="h5">{userDetails.bio || 'Sem Descrição'}</Typography>
+            <Box
+              display="flex"
+              flexWrap="wrap"
+              justifyContent={{
+                xs: 'flex-start',
+                md: 'space-between',
+              }}
+              mt={1}
+            >
+              <Box display="flex" alignItems="center" mt={1} minWidth={{ xs: '100%', md: '30%' }}>
+                <EmailIcon fontSize="small" />
+                <Typography variant="body1" ml={1}>
+                  {userDetails.email || 'Sem Email'}
+                </Typography>
+              </Box>
+              <Box display="flex" alignItems="center" mt={1} minWidth={{ xs: '100%', md: '30%' }}>
+                <PeopleIcon fontSize="small" />
+                <Typography variant="body1" ml={1}>
+                  {userDetails.followers} Followers
+                </Typography>
+              </Box>
+              <Box display="flex" alignItems="center" mt={1} minWidth={{ xs: '100%', md: '30%' }}>
+                <PersonAddIcon fontSize="small" />
+                <Typography variant="body1" ml={1}>
+                  {userDetails.following} Following
+                </Typography>
+              </Box>
+            </Box>
           </Grid>
         </Grid>
       </Box>
